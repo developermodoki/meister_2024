2f0da70d357b46afbfe2cf16845b1863a31ffdf8
Win.10-2 <61893884+developermodoki@users.noreply.github.com>
2024-11-10 17:45:53 +0900

    Updated some

--- atoms3_main/atoms3_main.ino
+++ atoms3_main/atoms3_main.ino
@@ -1,6 +1,7 @@
 #include <M5AtomS3.h>
 #include "ClosedCube_TCA9548A.h"
 #include <Wire.h>
+#include <WiFi.h>
 #include "UNIT_SCALES.h"
 
 #define HUB_ADDR 0x70
@@ -14,6 +15,7 @@ void setup() {
     Wire.begin(2, 1);
     tca9548a.address(HUB_ADDR);
 
+    // use Channel 2 and 3
     for(uint8_t ch = 1; ch < 3; ch++) {
       tca9548a.selectChannel(ch);
       while (!scales.begin(&Wire, 2, 1, DEVICE_DEFAULT_ADDR)) {
@@ -26,11 +28,15 @@ void setup() {
 void loop() {
 
   for(uint8_t ch = 1; ch < 3; ch++) {
+    int weight = 0;
     tca9548a.selectChannel(ch);
     Serial.print("Current Channel: ");
     Serial.println(ch);
+
+    weight = scales.getWeight();
     Serial.print("gram : ");
-    Serial.println(scales.getWeight());
+    Serial.println(weight);
     delay(500);
   }
 }
+
