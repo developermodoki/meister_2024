fe05fa06fc26db74136ef143db70c9f758681522
Win.10-2 <61893884+developermodoki@users.noreply.github.com>
2024-12-23 17:02:02 +0900

    Updated some bluetooth method

--- bluetooth_test/bluetooth_test.ino
+++ bluetooth_test/bluetooth_test.ino
@@ -1,9 +1,11 @@
 //https://github.com/espressif/arduino-esp32/blob/master/libraries/BLE/examples/Server_multiconnect/Server_multiconnect.ino
+//https://github.com/espressif/arduino-esp32/blob/master/libraries/BLE/examples/Write/Write.ino
 #include <M5AtomS3.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <BLE2902.h>
+
 
 #define BT_SV_NAME "m5-test"
 #define SERVICE_UUID        "e83823df-3f53-40cb-89f1-586c248ed29d"
@@ -11,21 +13,41 @@
 
 BLEServer *pServer = nullptr;
 BLECharacteristic *pCharacteristic = nullptr;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-uint32_t value = 0;
-
+bool Connected = false;
 
 class MyServerCallbacks : public BLEServerCallbacks {
   void onConnect(BLEServer *pServer) {
     BLEDevice::startAdvertising();
-    deviceConnected = true;
+    Connected = true;
+    Serial.println("Connected");
   };
 
   void onDisconnect(BLEServer *pServer) {
-    deviceConnected = false;
+    delay(500);         
+    BLEDevice::startAdvertising();
+    Connected = false;
+    Serial.println("Disconnected");
   }
 };
+
+
+class MyReceiveCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    std::string value = pCharacteristic->getValue();
+
+    if (value.length() > 0) {
+      Serial.println("*********");
+      Serial.print("New value: ");
+      for (int i = 0; i < value.length(); i++) {
+        Serial.print(value[i]);
+      }
+
+      Serial.println();
+      Serial.println("*********");
+    }
+  }
+};
+
 
 void setup() {
   AtomS3.begin(true);
@@ -34,13 +56,15 @@ void setup() {
   BLEDevice::init(BT_SV_NAME);
 
   pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
 
+  pServer->setCallbacks(new MyServerCallbacks());
   BLEService *pService = pServer->createService(SERVICE_UUID);
+
   pCharacteristic = pService->createCharacteristic(
     CHARACTERISTIC_UUID,
     BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY | BLECharacteristic::PROPERTY_INDICATE
   );
+  pCharacteristic->setCallbacks(new MyReceiveCallbacks());
 
   // https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.descriptor.gatt.client_characteristic_configuration.xml
   pCharacteristic->addDescriptor(new BLE2902());
@@ -50,34 +74,21 @@ void setup() {
   // Start advertising
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(false);
+  pAdvertising->setScanResponse(true);
   pAdvertising->setMinPreferred(0x0);
-  //pAdvertising->setMinPreferred(0x06); 
-  //pAdvertising->setMinPreferred(0x12);
+  pAdvertising->setMinPreferred(0x06); 
+  pAdvertising->setMinPreferred(0x12);
 
   BLEDevice::startAdvertising();
-  Serial.println("Waiting a client connection to notify...");
+  Serial.println("Bluetooth Slave Started");
 
 }
 
 void loop() {
-  if (deviceConnected) {
-    pCharacteristic->setValue((uint8_t *)&value, 4);
+  if (Connected) {
+    pCharacteristic->setValue("Hello World!");
     pCharacteristic->notify();
-    value++;
-    delay(10); 
-  }
-  
-  if (!deviceConnected && oldDeviceConnected) {
-    delay(500);                   
-    Serial.println("Disconnected");
-    pServer->startAdvertising();
-    oldDeviceConnected = deviceConnected;
-  }
-  
-  if (deviceConnected && !oldDeviceConnected) {
-    Serial.println("Connected");
-    oldDeviceConnected = deviceConnected;
+    delay(1000); 
   }
 }
 
